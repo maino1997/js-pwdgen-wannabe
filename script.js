7a43aed8375c02ff333fe90cc21db42d80d82a41
Sasha Mainardi <sashamainardi4@gmail.com>
2021-12-20 16:43:32 +0100

    analisi testo

---
+++ script.js
@@ -0,0 +1,11 @@
+// Chiedere Nome utente
+// nome in una variabile
+// Chiedere cognome utente
+// cognome in una variabile
+// Chiedere colore preferito
+// colore in una variabile
+// stampa nella console per verificare
+// Prendere elemento HTML dalla pagina
+// Iniettare contenuto stringa nella pagina
+
+
